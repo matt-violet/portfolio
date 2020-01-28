8a6eb844339bd7063a305d783e6c4d3f22730a58
mattviolet <mattviolet@gmail.com>
2020-01-28 15:20:04 -0800

    render menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <img :src="image" width=200 height=200/>
+    <div id="menu-wrapper">
+      <div class="menu-option">
+        About
+      </div>
+      <div class="menu-option">
+        Portfolio
+      </div>
+      <div class="menu-option">
+        Contact
+      </div>
+    </div>
+    <img :src="image"/>
   </div>
 </template>
 
@@ -9,11 +20,32 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      image: require('@/images/matt-violet.jpg')
+      image: require('@/images/oakland-sunset.jpg')
     }
   }
 }
 </script>
 
 <style scoped>
+img {
+  width: 100%;
+  height: 100%;
+  opacity: 0.5;
+}
+#menu-wrapper {
+  background-color: lightblue;
+  text-align: center;
+  height: 40px;
+  width: 100%;
+}
+.menu-option {
+  background-color: lightblue;
+  display: inline-block;
+  vertical-align: middle;
+  width: 100px;
+  height: 100%;
+}
+.menu-option:hover {
+  background-color: lightcyan;
+}
 </style>
