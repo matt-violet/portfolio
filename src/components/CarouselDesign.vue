028e697b92e2bdb350cb7dec370e08c9f89e5086
mattviolet <mattviolet@gmail.com>
2020-02-14 20:41:28 -0800

    Add design carousel, without viewProjectDetails functionality

---
+++ src/components/CarouselDesign.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="carousel">
+    <img class="arrow back" src="../images/left-arrow.png" v-on:click="handleBack">
+    <div class="track-container">
+      <div class="track-inner">
+        <img
+          :src="designPortfolio[currentSlide].image"
+          class='img'
+          v-on:click="$emit('viewProjectDetails', designPortfolio[currentSlide])"
+        />
+      </div>
+    </div>
+        <p>{{ designPortfolio[currentSlide].description }}</p>
+    <img class="arrow next" src="../images/right-arrow.png" v-on:click="handleNext">
+    <div class="carousel-nav">
+      <button class="carousel-indicator" :class="currentSlide === 0 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(0)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 1 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(1)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 2 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(2)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 3 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(3)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 4 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(4)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 5 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(5)"></button>
+      <button class="carousel-indicator" :class="currentSlide === 6 ? 'current-slide' : ''" v-on:click="handleNavIndicatorClick(6)"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarouselDesign',
+  props: {
+    viewProjectDetails: { type: Function },
+    project: { type: Object },
+  },
+  methods: {
+    handleNext: function() {
+      if (this.currentSlide < 6) {
+        this.currentSlide++
+      }
+    },
+    handleBack: function() {
+      if (this.currentSlide > 0) {
+        this.currentSlide--
+      }
+    },
+    handleNavIndicatorClick: function(index) {
+      this.currentSlide = index;
+    }
+  },
+  data() {
+    return {
+      currentSlide: 0,
+      designPortfolio: [
+        {
+          image: require('../images/GA2016-flyer.png'),
+          description: 'Ad in UU World Magazine, 2016',
+          design: true
+        },
+        {
+          image: require('../images/GA2017-flyer.png'),
+          description: 'Ad in UU World Magazine, 2017',
+          design: true
+        },
+        {
+          image: require('../images/GA2018-flyer.png'),
+          description: 'Ad in UU World Magazine, 2018',
+          design: true
+        },
+        {
+          image: require('../images/Symposium2019.jpg'),
+          description: 'Event Flyer, 2019',
+          design: true
+        },
+        {
+          image: require('../images/GA2017.png'),
+          description: 'Recruitment Flyer, 2018',
+          design: true
+        },
+        {
+          image: require('../images/SEAsia.png'),
+          description: 'Personal Side Project, 2015',
+          design: true
+        },
+        {
+          image: require('../images/SKSM-banner.jpg'),
+          description: '8-foot Retractable Banner, 2017',
+          design: true
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h2 {
+    margin: 0;
+  }
+  p {
+    margin:  0;
+    line-height: 1.5;
+  }
+  .carousel {
+    width: 500px;
+    height: 500px;
+    position: relative;
+    margin: 0 auto;
+    text-align: center;
+    background: white;
+  }
+  .track-container {
+    margin: 0 auto;
+    padding: 10px;
+    height: 83%;
+    display: flex;
+    align-items: center;
+  }
+  .track-inner {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    margin: auto;
+  }
+  .img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: fill;
+  }
+  .img:hover {
+    cursor: pointer;
+  }
+  .arrow {
+    position: absolute;
+    top: 50%;
+    width: 15px;
+  }
+  .arrow:hover {
+    cursor: pointer;
+  }
+  .back {
+    left: -40px;
+  }
+  .next {
+    right: -40px;
+  }
+  .carousel-nav {
+    height: 40px;
+    width: 100%;
+    text-align: center;
+    position : absolute;
+    bottom: 0;
+  }
+  .carousel-indicator {
+    background: lightgray;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    margin: 5px;
+  }
+  .carousel-indicator:hover {
+    cursor: pointer;
+  }
+  .carousel-indicator.current-slide {
+    background: gray;
+  }
+</style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,45 +5,22 @@
         View Resume
       </a>
       <h1 class="projects-header">Software Projects</h1>
-        <CarouselSoftware class='carousel' :project="project" @viewProjectDetails="viewProjectDetails"/>
-
-      <!-- <div class="projects-wrapper">
-        <div
-          v-for="(project, i) in softwarePortfolio" 
-          v-on:click="$emit('viewProjectDetails', project)"
-          v-bind:key="i"
-          class="project-div"
-        >
-          <div class="img-div">
-            <img :src="project.image" class='img'/>
-          </div>
-          <h2>{{ project.title }}</h2>
-          <p>{{ project.description }}</p>
-        </div>
-      </div> -->
-
-      <div class="design-wrapper">
-        <h1 class="design-header">Graphic Design</h1>
-        <div v-for="(project, i) in designPortfolio"
-          v-on:click="$emit('viewProjectDetails', project)"
-          v-bind:key="i"
-          class="design-project-div"
-        >
-          <img :src="project.image" class="design-imgs"/>
-          <p>{{ project.description }}</p>
-        </div>
-      </div>
+      <CarouselSoftware :project="project" @viewProjectDetails="viewProjectDetails"/>
+      <h1 class="design-header">Graphic Design</h1>
+      <CarouselDesign :project="project" @viewProjectDetails="viewProjectDetails"/>
     </div>
   </div>
 </template>
 
 <script>
 import CarouselSoftware from './CarouselSoftware.vue'
+import CarouselDesign from './CarouselDesign.vue'
 
 export default {
   name: 'Portfolio',
   components: {
     CarouselSoftware,
+    CarouselDesign,
   },
   props: {
     viewProjectDetails: {type: Function},
@@ -51,77 +28,77 @@ export default {
   },
   data() {
     return {
-      softwarePortfolio: {
-        OpenRestaurant: {
-          title: 'Open Restaurant',
-          description: 'Responsive photo gallery for a restaurant review app',
-          details: 'Welcome to the photos module for Open Restaurant! The photos module presents the user with an image gallery for a given restaurant. The photos module enables full-screen modal viewing of each gallery photo with responsive sizing, associated photo details, and intuitive navigation of gallery photos. The "restaurant_id" is determined by the 1-8 digit number included at the end of the url. When the gallery page component mounts, the restuarant_id is sent in a GET request to an Express server that queries a MongoDB database for image urls of photos stored on Amazon Web Services.',
-          stack: ' HTML, CSS, Javascript, React, Styled-Components, Express, MongoDB, Faker, Jest, Enzyme',
-          image: require('../images/open-restaurant.png'),
-          github: 'https://github.com/krummurk/photos-module'
-        },
-        SocialInn: {
-          title: 'Social Inn',
-          description: 'Scaled the back end of a rental accommodations app',
-          details: 'Social-Inn is an online marketplace for users to book or offer lodging. This module displays all reviews associated with a particular room. I scaled this module\'s back end to handle 10 million records and 100 requests per second. I benchmarked performance of a SQL vs. NoSQL database with 10M records to determine optimal database. I deployed app on AWS and stress tested server to identify performance bottlenecks.',
-          stack: 'PostgreSQL, Cassandra, Express, Amazon Web Services, k6, New Relic',
-          image: require('../images/couch.jpg'),
-          github: 'https://github.com/social-inn/Reviews'
-        },
-        connect4: {
-          title: 'Connect Four',
-          description: 'Game in which two players drop discs into slots and connect four to win',
-          details: 'This app is modeled after the popular "Connect Four" game published by Milton Bradley in 1974 in which two players alternate dropping pieces into a 7x7 board trying to occupy 4 adjacent spaces. The app utilizes CSS animations and a dynamically rendering board (based on a two dimensional array), game status, head-to-head score, and rematch button.',
-          stack: 'HTML, CSS, Javascript, React',
-          image: require('../images/connect-four.png'),
-          github: 'https://github.com/matt-violet/connect4'
-        },
-        MyBolus: {
-          title: 'My Bolus',
-          description: 'Calculating the proper dose of medication can be tough for diabetics. My Bolus makes it easy!',
-          details: 'This app simulates modern insulin pump technology by implementing a complex algorithm allowing users to calculate their bolus based on meal choice, current blood glucose (bg) level, insulin-to-carb ratio, bg correction factor, future exercise plans, and other factors.',
-          stack: 'HTML, CSS, Javascript, React, Express, MongoDB',
-          image: require('../images/math.jpg'),
-          github: 'https://github.com/matt-violet/My-Bolus'
-        }
-      },
-      designPortfolio: {
-        ga2016: {
-          image: require('../images/GA2016-flyer.png'),
-          description: 'Ad in UU World Magazine, 2016',
-          design: true
-        },
-        ga2017: {
-          image: require('../images/GA2017-flyer.png'),
-          description: 'Ad in UU World Magazine, 2017',
-          design: true
-        },
-        ga2018: {
-          image: require('../images/GA2018-flyer.png'),
-          description: 'Ad in UU World Magazine, 2018',
-          design: true
-        },
-        ga2019: {
-          image: require('../images/Symposium2019.jpg'),
-          description: 'Event Flyer, 2019',
-          design: true
-        },
-        symposium2019: {
-          image: require('../images/GA2017.png'),
-          description: 'Recruitment Flyer, 2018',
-          design: true
-        },
-        SEAsia: {
-          image: require('../images/SEAsia.png'),
-          description: 'Personal Side Project, 2015',
-          design: true
-        },
-        sksmBanner: {
-          image: require('../images/SKSM-banner.jpg'),
-          description: '8-foot Retractable Banner, 2017',
-          design: true
-        },
-      }
+      // softwarePortfolio: {
+      //   OpenRestaurant: {
+      //     title: 'Open Restaurant',
+      //     description: 'Responsive photo gallery for a restaurant review app',
+      //     details: 'Welcome to the photos module for Open Restaurant! The photos module presents the user with an image gallery for a given restaurant. The photos module enables full-screen modal viewing of each gallery photo with responsive sizing, associated photo details, and intuitive navigation of gallery photos. The "restaurant_id" is determined by the 1-8 digit number included at the end of the url. When the gallery page component mounts, the restuarant_id is sent in a GET request to an Express server that queries a MongoDB database for image urls of photos stored on Amazon Web Services.',
+      //     stack: ' HTML, CSS, Javascript, React, Styled-Components, Express, MongoDB, Faker, Jest, Enzyme',
+      //     image: require('../images/open-restaurant.png'),
+      //     github: 'https://github.com/krummurk/photos-module'
+      //   },
+      //   SocialInn: {
+      //     title: 'Social Inn',
+      //     description: 'Scaled the back end of a rental accommodations app',
+      //     details: 'Social-Inn is an online marketplace for users to book or offer lodging. This module displays all reviews associated with a particular room. I scaled this module\'s back end to handle 10 million records and 100 requests per second. I benchmarked performance of a SQL vs. NoSQL database with 10M records to determine optimal database. I deployed app on AWS and stress tested server to identify performance bottlenecks.',
+      //     stack: 'PostgreSQL, Cassandra, Express, Amazon Web Services, k6, New Relic',
+      //     image: require('../images/couch.jpg'),
+      //     github: 'https://github.com/social-inn/Reviews'
+      //   },
+      //   connect4: {
+      //     title: 'Connect Four',
+      //     description: 'Game in which two players drop discs into slots and connect four to win',
+      //     details: 'This app is modeled after the popular "Connect Four" game published by Milton Bradley in 1974 in which two players alternate dropping pieces into a 7x7 board trying to occupy 4 adjacent spaces. The app utilizes CSS animations and a dynamically rendering board (based on a two dimensional array), game status, head-to-head score, and rematch button.',
+      //     stack: 'HTML, CSS, Javascript, React',
+      //     image: require('../images/connect-four.png'),
+      //     github: 'https://github.com/matt-violet/connect4'
+      //   },
+      //   MyBolus: {
+      //     title: 'My Bolus',
+      //     description: 'Calculating the proper dose of medication can be tough for diabetics. My Bolus makes it easy!',
+      //     details: 'This app simulates modern insulin pump technology by implementing a complex algorithm allowing users to calculate their bolus based on meal choice, current blood glucose (bg) level, insulin-to-carb ratio, bg correction factor, future exercise plans, and other factors.',
+      //     stack: 'HTML, CSS, Javascript, React, Express, MongoDB',
+      //     image: require('../images/math.jpg'),
+      //     github: 'https://github.com/matt-violet/My-Bolus'
+      //   }
+      // },
+      // designPortfolio: {
+      //   ga2016: {
+      //     image: require('../images/GA2016-flyer.png'),
+      //     description: 'Ad in UU World Magazine, 2016',
+      //     design: true
+      //   },
+      //   ga2017: {
+      //     image: require('../images/GA2017-flyer.png'),
+      //     description: 'Ad in UU World Magazine, 2017',
+      //     design: true
+      //   },
+      //   ga2018: {
+      //     image: require('../images/GA2018-flyer.png'),
+      //     description: 'Ad in UU World Magazine, 2018',
+      //     design: true
+      //   },
+      //   ga2019: {
+      //     image: require('../images/Symposium2019.jpg'),
+      //     description: 'Event Flyer, 2019',
+      //     design: true
+      //   },
+      //   symposium2019: {
+      //     image: require('../images/GA2017.png'),
+      //     description: 'Recruitment Flyer, 2018',
+      //     design: true
+      //   },
+      //   SEAsia: {
+      //     image: require('../images/SEAsia.png'),
+      //     description: 'Personal Side Project, 2015',
+      //     design: true
+      //   },
+      //   sksmBanner: {
+      //     image: require('../images/SKSM-banner.jpg'),
+      //     description: '8-foot Retractable Banner, 2017',
+      //     design: true
+      //   },
+      // }
     }
   }  
 }
