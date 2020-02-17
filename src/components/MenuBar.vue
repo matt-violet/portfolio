63bca0cd8af5886e8bbf55d14325c1eabf7d62f6
mattviolet <mattviolet@gmail.com>
2020-02-16 20:36:35 -0800

    adjust menubar transparency

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -70,7 +70,7 @@ export default {
     position: absolute;
     text-align: center;
     color: gray;
-    background: black;
+    background: rgb(0, 0, 0, .9);
     /* border-bottom: solid 1px gray; */
     transition: 0.3s all ease-out;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
